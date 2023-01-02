a26e7955601664b5790b67b7b20e1c8ea3f5d461
Jemima Nyambura Osoro <42281543+Gemmy2010@users.noreply.github.com>
2023-01-02 21:41:06 +0100

    filter sort table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import {MatPaginator } from '@angular/material/paginator'
+import {MatTableDataSource } from '@angular/material/table'
+import { DataService } from './data.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'material-demo';
+   
+  displayedColumns: string[] = ['name','username','email'];
+  dataSource!:MatTableDataSource<any>;
+
+  @ViewChild('paginator') paginator! : MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+
+
+
+
+
+  constructor(private service: DataService){}
+
+
+  ngOnInit(){
+    this.service.getUserData().subscribe((response:any) =>{
+      this.dataSource = new MatTableDataSource(response);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
+      console.log(response)
+    })
+  }
+
+
+  applyFilter($event: any){
+    this.dataSource.filter = $event.target.value;
+  }
+  
+  
+    
+  
+  // showSpinner=false;
+
+  // loadData(){
+  //   this.showSpinner = true;
+
+  //   setTimeout(() => {
+  //     this.showSpinner = false;
+  //   }, 5000);
+  // }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatTableModule } from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {HttpClientModule} from '@angular/common/http';
+import {MatSortModule} from '@angular/material/sort'
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatSortModule
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
